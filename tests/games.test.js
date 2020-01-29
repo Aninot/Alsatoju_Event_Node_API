58737a28bba3d9b11c27d7e5384d2a633b1ec879
Thomas A <aninothomas@hotmail.fr>
2020-01-29 16:33:21 +0100

    Delete game test

--- tests/games.test.js
+++ tests/games.test.js
@@ -1,20 +1,37 @@
 const expect = require('expect');
 const request = require('supertest');
 const { app } = require('./../server');
-const { game } = require('../models/Game.model');
+const { Game } = require('../models/Game.model');
 
+
+const game = {
+                id : null,
+                name : "TestGame1",
+                url : "TestUrl1"
+             };
 
 describe('POST /games', () => {
 
   it('doit créer un nouveau game', (done) => {
     request(app)
       .post('/games')
-      .send({name:"Test game name", url: "Test game url"})
+      .send({name: game.name, url: game.url})
       .expect(201)
       .expect(res => {
-        expect(res.body.name).toBe("Test game name")
-        expect(res.body.url).toBe("Test game url")
+        expect(res.body.name).toBe(game.name)
+        expect(res.body.url).toBe(game.url)
+        game.id = res.body.id
       })
+      .end(done);
+  })
+});
+
+describe('DELETE /games', () => {
+
+  it('doit supprimer un game', (done) => {
+    request(app)
+      .delete('/games/' + game.id)
+      .expect(204)
       .end(done);
   })
 });
