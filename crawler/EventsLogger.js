6acd8da14a857902201c4204115c6e1d03a204f2
Alan <m.limaalan@hotmail.fr>
2020-01-31 14:08:20 +0100

    fix filename

--- crawler/EventsLogger.js
+++ crawler/EventsLogger.js
@@ -1,22 +1,21 @@
 const fs = require('fs');
 
-let filename = new Date();
+let filename = new Date().toString();
 let content = ' événements enregistrés !';
 
-var logger = function(count, start_date, end_date) {
-    fs.writeFile('crawler/EventsLogs/' 
-    	+ filename 
-    	+ '.txt', start_date 
-    	+ ' - ' 
-    	+ end_date 
-    	+ '\n'
-    	+ count
-    	+ content, (err) => {
-        if(err) {
-            console.error(err);
+var logger = function (count, start_date, end_date) {
+  fs.writeFile('crawler/EventsLogs/' + filename.replace(' ', '_') + '.txt',
+    start_date
+    + ' - '
+    + end_date
+    + '\n'
+    + count
+    + content, (err) => {
+      if (err) {
+        console.error(err);
 
-        }
+      }
     }
-)
+  )
 };
 module.exports = logger;
