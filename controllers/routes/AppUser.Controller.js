46cf20d368ab1abab12161954a0e35e466ee50af
Alan <alan@hypecode.com>
2020-03-23 17:49:27 +0100

    start documentation and add a test on /login

--- controllers/routes/AppUser.Controller.js
+++ controllers/routes/AppUser.Controller.js
@@ -1,10 +1,46 @@
 const AppUserLib = require('../lib/AppUser.lib');
 
 module.exports = function (app) {
-    // GET /app_users
+    /**
+     * @api {get} /app_users request all users informations
+     * @apiName GetAppUsers
+     * @apiGroup AppUser
+     * 
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     [
+     *      {
+     *          "firstname": "John",
+     *          "lastname": "Doe"
+     *      },
+     *      ...
+     *     ]
+     * 
+     */
     app.get('/app_users', AppUserLib.getAll)
 
-    // GET /app_users/:id
+    /**
+     * @api {get} /app_users/:id request one user informations
+     * @apiName GetAppUser
+     * @apiGroup AppUser
+     * 
+     * @apiParam {Number} id User's unique ID.
+     * 
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "firstname": "John",
+     *       "lastname": "Doe"
+     *     }
+     * 
+     * @apiError UserNotFound The id of the User was not found.
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "error": "UserNotFound"
+     *     }
+     */
     app.get('/app_users/:id', AppUserLib.getOne)
 
     // POST /app_users
--- controllers/routes/Game.Controller.js
+++ controllers/routes/Game.Controller.js
@@ -1,10 +1,46 @@
 const GameLib = require('../lib/Game.lib');
 
 module.exports = function (app) {
-    // GET /games
+    /**
+     * @api {get} /games request all games informations
+     * @apiName GetGames
+     * @apiGroup Game
+     * 
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     [
+     *      {
+     *          "key": "value",
+     *          "otherKey": "otherValue"
+     *      },
+     *      ...
+     *     ]
+     * 
+     */
     app.get('/games', GameLib.getAll)
 
-    // GET /games/:id
+    /**
+     * @api {get} /games/:id request one user informations
+     * @apiName GetGame
+     * @apiGroup Game
+     * 
+     * @apiParam {Number} id game's unique ID.
+     * 
+     * @apiSuccessExample Success-Response:
+     *     HTTP/1.1 200 OK
+     *     {
+     *       "key": "value",
+     *       "otherKey": "otherValue"
+     *     }
+     * 
+     * @apiError GameNotFound The id of the Game was not found.
+     *
+     * @apiErrorExample Error-Response:
+     *     HTTP/1.1 404 Not Found
+     *     {
+     *       "error": "GameNotFound"
+     *     }
+     */
     app.get('/games/:id', GameLib.getOne)
 
     // POST /games
