be8fa02af7469bd846543d88a43199570d232b6e
Alan <alan@hypecode.com>
2020-05-12 15:59:30 +0200

    remove need to be authentified when register

--- controllers/routes/AppUser.Controller.js
+++ controllers/routes/AppUser.Controller.js
@@ -1,21 +1,20 @@
-const AppUserLib = require('../lib/AppUser.lib');
-const JwtMiddleware = require('../../services/Authentification.Service');
+const AppUserLib = require('../lib/AppUser.lib')
+const JwtMiddleware = require('../../services/Authentification.Service')
 
 module.exports = function (app) {
-    
-    app.get('/app_users', JwtMiddleware.isAuthenticated, AppUserLib.getAll)
-    
-    app.get('/app_users/:id', JwtMiddleware.isAuthenticated, AppUserLib.getOne)
-    
-    app.post('/app_users', JwtMiddleware.isAuthenticated, AppUserLib.postAppUser)
-    
-    app.patch('/app_users/:id', JwtMiddleware.isAuthenticated, AppUserLib.patchAppUser)
-    
-    app.delete('/app_users/:id', JwtMiddleware.isAuthenticated, AppUserLib.deleteAppUser)
-    
-    app.post('/login', AppUserLib.postLogin)
+  app.get('/app_users', JwtMiddleware.isAuthenticated, AppUserLib.getAll)
 
-    app.get('/reset_password', AppUserLib.resetPassword)
-    
-    app.patch('/reset_password', AppUserLib.patchPassword)
+  app.get('/app_users/:id', JwtMiddleware.isAuthenticated, AppUserLib.getOne)
+
+  app.post('/app_users', AppUserLib.postAppUser)
+
+  app.patch('/app_users/:id', JwtMiddleware.isAuthenticated, AppUserLib.patchAppUser)
+
+  app.delete('/app_users/:id', JwtMiddleware.isAuthenticated, AppUserLib.deleteAppUser)
+
+  app.post('/login', AppUserLib.postLogin)
+
+  app.get('/reset_password', AppUserLib.resetPassword)
+
+  app.patch('/reset_password', AppUserLib.patchPassword)
 }
